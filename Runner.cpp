edd6df2b28910b6520c5de0acd1a2ef5df519cb6
bvanchuhov <bodya.van@gmail.com>
2025-12-03 06:26:31 +0000

    Day 3 - https://adventofcode.com/2025/day/3

--- Runner.cpp
+++ Runner.cpp
@@ -1,10 +1,10 @@
-#include "day2/Day2.h"
+#include "day3/Day3.h"
 #include <iostream>
 #include <boost/stacktrace.hpp>
 
 int main() {
     try {
-        day2::run();
+        day3::run();
     } catch (const std::exception& e) {
         const boost::stacktrace::stacktrace trace = boost::stacktrace::stacktrace::from_current_exception();
         std::cerr << "Caught exception: " << e.what() << "\n" << trace;
---
+++ day3/Day3.cpp
@@ -0,0 +1,64 @@
+#include "Day3.h"
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+namespace day3 {
+
+    using namespace std::string_literals;
+
+    int run() {
+        const std::filesystem::path inputPath = "../day3/input.txt";
+
+        std::ifstream file1(inputPath);
+        const auto res1 = solve1(file1);
+        std::cout << "Solution 1: " << res1 << std::endl;
+
+        std::ifstream file2(inputPath);
+        const auto res2 = solve2(file2);
+        std::cout << "Solution 2: " << res2 << std::endl;
+        return 0;
+    }
+
+    std::pair<long, int> calcJoltage(const std::string_view& s, int digits) {
+        if (s.empty() || digits == 0) {
+            return std::make_pair(0, digits);
+        }
+
+        const auto firstMaxIdx = std::distance(s.begin(), std::ranges::max_element(s));
+        const auto [rightJoltage, remainingDigits] =
+            calcJoltage(s.substr(firstMaxIdx + 1, s.length() - firstMaxIdx), digits - 1);
+        const auto [leftJoltage, remainingDigits2] =
+            calcJoltage(s.substr(0, firstMaxIdx), remainingDigits);
+
+        std::string res;
+        if (leftJoltage != 0) {
+            res += std::to_string(leftJoltage);
+        }
+        res += s[firstMaxIdx];
+        if (rightJoltage != 0) {
+            res += std::to_string(rightJoltage);
+        }
+
+        return std::make_pair(std::stol(res), remainingDigits2);
+    }
+
+    long solve(std::istream& in, const int digits) {
+        long result = 0;
+        for (std::string line; std::getline(in, line);) {
+            auto [joltage, _] = calcJoltage(line, digits);
+            result += joltage;
+        }
+        return result;
+    }
+
+    long solve1(std::istream& in) {
+        return solve(in, 2);
+    }
+
+    long solve2(std::istream& in) {
+        return solve(in, 12);
+    }
+}
